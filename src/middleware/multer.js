8301b8911533a1a7688015c340089e24a43adc13
Martin Ferreira <martin.f.yic@gmail.com>
2023-02-12 13:08:31 -0300

    feat: :sparkles: Add multer.js

---
+++ src/middleware/multer.js
@@ -0,0 +1,31 @@
+import multer from 'multer';
+import { dirname, extname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const mimeTypes = ['image/jpeg', 'image/png'];
+
+export const upload = multer({
+	storage: multer.diskStorage({
+		destination: join(currentDir, '../views/uploads/userAvatar'),
+		filename: (req, file, cb) => {
+			const fileExtension = extname(file.originalname);
+			//TODO ver si no tiene la extencion ponerle a mano
+			//? mimetype: 'image/jpeg' --> ver separar split <============!!
+
+			const fileName = file.originalname.split(fileExtension)[0];
+
+			cb(null, `${fileName}-${Date.now()}${fileExtension}`);
+		},
+	}),
+	fileFilter: (req, file, cb) => {
+		if (mimeTypes.includes(file.mimetype)) cb(null, true);
+		else
+			cb(
+				new Error(`El formato de las imagenes debe ser ${mimeTypes.join(' ')}`)
+			);
+	},
+	limits: {
+		fieldSize: 10000000,
+	},
+});
