607e95cdfe29dffc64ecb9b9a40e0f85e22a6c50
Martin Ferreira <martin.f.yic@gmail.com>
2023-02-12 18:49:25 -0300

    feat: :sparkles: Add new endpoints in src/routes/v1/userRoutes.js

--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -1,18 +1,29 @@
 import { Router } from 'express';
+import passport from 'passport';
 import { upload } from '../../middleware/multer.js';
 import * as userController from '../../controllers/userController.js';
+import { isAuth } from '../../middleware/isAuth.js';
 
 const v1UserRouter = Router();
 
 v1UserRouter
-	.get('/', userController.getAllUsers)
-	.get('/id/:id', userController.getUserById)
-	.get('/signup', (req, res) => {
-		res.render('signup');
-	})
-	.post('/', upload.single('avatar'), userController.createNewUser)
-	.get('/login', (req, res) => {
-		res.render('login');
-	});
+	.get('/', isAuth, userController.getAllUsers)
+	.get('/id/:id', isAuth, userController.getUserById)
+	.get('/signup', userController.singUpUser)
+	.post('/signup', upload.single('avatar'), userController.createNewUser)
+	.get('/login', userController.loginUser)
+	.post(
+		'/login',
+		passport.authenticate('login', {
+			failureRedirect: '/users/error-login',
+		}),
+		(req, res) => {
+			res.redirect('/users/menu');
+		}
+	)
+	.get('/logout', isAuth, userController.userLogOut)
+	.get('/menu', isAuth, userController.userMenu);
+
+v1UserRouter.get('/error-login', userController.loginError);
 
 export default v1UserRouter;
