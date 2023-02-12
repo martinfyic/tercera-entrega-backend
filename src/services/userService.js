0f07634e427f463508f839b385e32b2e6fcc4ba5
Martin Ferreira <martin.f.yic@gmail.com>
2023-02-12 13:15:25 -0300

    feat: :sparkles: Add userServices.js

---
+++ src/services/userService.js
@@ -0,0 +1,16 @@
+import * as Users from '../daos/users/UsersDAOMongo.js';
+
+export const getAllUsers = async (limit, since) => {
+	const allUsers = await Users.getAllUsers(limit, since);
+	return allUsers;
+};
+
+export const getUserById = async id => {
+	const userById = await Users.getUserById(id);
+	return userById;
+};
+
+export const createNewUser = async newUser => {
+	const savedUser = await Users.createNewUser(newUser);
+	return savedUser;
+};
