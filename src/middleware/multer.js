6b286d0f6cd46a7ef182081dc92bb7973c64c4cd
Martin Ferreira <martin.f.yic@gmail.com>
2023-02-13 20:25:06 -0300

    feat: :zap: Multer to add products

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -29,3 +29,28 @@ export const upload = multer({
 		fieldSize: 10000000,
 	},
 });
+
+export const productsUpload = multer({
+	storage: multer.diskStorage({
+		destination: join(currentDir, '../../public/uploads/products'),
+		filename: (req, file, cb) => {
+			const fileExtension = extname(file.originalname);
+			//TODO ver si no tiene la extencion ponerle a mano
+			//? mimetype: 'image/jpeg' --> ver separar split <============!!
+
+			const fileName = file.originalname.split(fileExtension)[0];
+
+			cb(null, `${fileName}-${Date.now()}${fileExtension}`);
+		},
+	}),
+	fileFilter: (req, file, cb) => {
+		if (mimeTypes.includes(file.mimetype)) cb(null, true);
+		else
+			cb(
+				new Error(`El formato de las imagenes debe ser ${mimeTypes.join(' ')}`)
+			);
+	},
+	limits: {
+		fieldSize: 10000000,
+	},
+});
