c71be79a935dd4e2e3af5448650dc308f74fc32d
Martin Ferreira <martin.f.yic@gmail.com>
2023-02-17 17:26:01 -0300

    feat: :zap: refactor include user info when save de cart

--- src/schemas/cartsSchema.js
+++ src/schemas/cartsSchema.js
@@ -3,6 +3,9 @@ import mongoos from 'mongoose';
 const cartsSchema = mongoos.Schema(
 	{
 		products: Array,
+		name: { type: String, required: true },
+		lastname: { type: String, required: true },
+		userId: { type: String, required: true },
 	},
 	{ timestamps: true, versionKey: false }
 );
