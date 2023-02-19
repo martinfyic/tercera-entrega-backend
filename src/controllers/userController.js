e39de0f827e98bff28583f97e84a7268636bd394
Martin Ferreira <martin.f.yic@gmail.com>
2023-02-19 18:58:23 -0300

    refactor: :fire: remove nodemailer transporter
    move nodemailer to userService.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,4 @@
 import * as userService from '../services/userService.js';
-import { transporter } from '../config/nodemailer.js';
 
 export const getAllUsers = async (req, res) => {
 	const { limit, since } = req.query;
@@ -55,44 +54,6 @@ export const createNewUser = async (req, res) => {
 	} else {
 		try {
 			await userService.createNewUser(newUser);
-
-			await transporter.sendMail({
-				from: `"Nodemailer ⚡" <${process.env.NODEMAILER_EMAIL}>`,
-				to: `${process.env.NODEMAILER_EMAIL_ADMIN}`,
-				subject: 'Nuevo registro en DB ✔',
-				html: `
-					<h1>Nuevo registro en DB 🔥</h1>
-					<hr/>
-					<h3>Informacion:</h3>
-					<ul>
-						<li>
-							<p><strong>Nombre:</strong> ${body.first}</p>
-						</li>
-						<li>
-							<strong>Apellido:</strong> ${body.last}</p>
-						</li>
-						<li>
-							<strong>Edad:</strong> ${body.age}</p>
-						</li>
-						<li>
-							<strong>Email:</strong> ${body.email}</p>
-						</li>
-						<li>
-							<strong>Telefono:</strong> ${body.phone}</p>
-						</li>
-						<li>
-							<strong>Calle:</strong> ${body.street}</p>
-						</li>
-						<li>
-							<strong>Numero de calle:</strong> ${body.streetNum}</p>
-						</li>
-						<li>
-							<strong>Numero de Apartamento:</strong> ${body.departmentNum}</p>
-						</li>
-					</ul>
-					<hr/>
-					`,
-			});
 
 			res.redirect('/users/login');
 		} catch (error) {
