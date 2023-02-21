bc2e1a1a2f0bf132b076c752f67c672ca090a6cd
Martin Ferreira <martin.f.yic@gmail.com>
2023-02-21 17:43:02 -0300

    chore: :mute: remove clg from twilio sendWhatsapp

--- src/config/twilio.js
+++ src/config/twilio.js
@@ -7,12 +7,11 @@ const client = twilio(accountSid, authToken);
 
 export const sendWhatsapp = async (userPhone, OrderNum) => {
 	try {
-		const message = await client.messages.create({
+		await client.messages.create({
 			body: `Su pedido Nro: *${OrderNum}* fue recibido 📦`,
 			from: process.env.TWILIO_PHONE,
 			to: userPhone,
 		});
-		console.log(message);
 	} catch (error) {
 		console.log(error);
 	}
