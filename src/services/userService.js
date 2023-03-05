f09a82cd006817a65b8eb8801f3979f8c882ba80
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-05 20:00:12 -0300

    refactor: :art: create singupEmail(  ) to seaparate logic

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,61 +1,40 @@
 import * as Users from '../daos/users/UsersDAOMongo.js';
-import { transporter } from '../config/nodemailer.js';
+import { singupEmail } from '../templates/singupEmail.js';
+import { logger } from '../config/index.js';
 
 export const getAllUsers = async (limit, since) => {
-	const allUsers = await Users.getAllUsers(limit, since);
-	return allUsers;
+	try {
+		const allUsers = await Users.getAllUsers(limit, since);
+		return allUsers;
+	} catch (error) {
+		logger.error(error);
+	}
 };
 
 export const getUserById = async id => {
-	const userById = await Users.getUserById(id);
-	return userById;
+	try {
+		const userById = await Users.getUserById(id);
+		return userById;
+	} catch (error) {
+		logger.error(error);
+	}
 };
 
-export const createNewUser = async newUser => {
-	const savedUser = await Users.createNewUser(newUser);
-
-	await transporter.sendMail({
-		from: `"Nodemailer ⚡" <${process.env.NODEMAILER_EMAIL}>`,
-		to: `${process.env.NODEMAILER_EMAIL_ADMIN}`,
-		subject: 'Nuevo registro en DB ✔',
-		html: `
-			<h1>Nuevo registro en DB 🔥</h1>
-			<hr/>
-			<h3>Informacion:</h3>
-			<ul>
-				<li>
-					<p><strong>Nombre:</strong> ${newUser.name.first}</p>
-				</li>
-				<li>
-					<strong>Apellido:</strong> ${newUser.name.last}</p>
-				</li>
-				<li>
-					<strong>Edad:</strong> ${newUser.age}</p>
-				</li>
-				<li>
-					<strong>Email:</strong> ${newUser.email}</p>
-				</li>
-				<li>
-					<strong>Telefono:</strong> ${newUser.phone}</p>
-				</li>
-				<li>
-					<strong>Calle:</strong> ${newUser.address.street}</p>
-				</li>
-				<li>
-					<strong>Numero de calle:</strong> ${newUser.address.streetNum}</p>
-				</li>
-				<li>
-					<strong>Numero de Apartamento:</strong> ${newUser.address.departmentNum}</p>
-				</li>
-			</ul>
-			<hr/>
-			`,
-	});
-
-	return savedUser;
+export const createNewUser = async (body, avatar) => {
+	try {
+		const savedUser = await Users.createNewUser(body, avatar);
+		await singupEmail(body);
+		return savedUser;
+	} catch (error) {
+		logger.error(error);
+	}
 };
 
 export const createUserCart = async user => {
-	const userCart = await Users.createUserCart(user);
-	return userCart;
+	try {
+		const userCart = await Users.createUserCart(user);
+		return userCart;
+	} catch (error) {
+		logger.error(error);
+	}
 };
