6181eff22ee237dfa1635bcf0540d81edf455b06
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-05 20:03:24 -0300

    refactor: :recycle: separate the logic of sending registration mail

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,50 +15,16 @@ export const getUserById = async (req, res) => {
 };
 
 export const createNewUser = async (req, res) => {
-	const { body } = req;
-	const avatar = req.file ? req.file.filename : 'default-image.png';
+	try {
+		const { body } = req;
+		const userImage = req.file ? req.file.filename : 'default-image.png';
+		const avatar = `${req.protocol}://${req.get('host')}/avatar/${userImage}`;
 
-	const newUser = {
-		name: {
-			first: body.first,
-			last: body.last,
-		},
-		email: body.email,
-		password: body.password,
-		avatar: `${req.protocol}://${req.get('host')}/avatar/${avatar}`,
-		phone: body.phone,
-		address: {
-			street: body.street,
-			streetNum: body.streetNum,
-			departmentNum: body.departmentNum,
-		},
-		age: body.age,
-	};
+		await userService.createNewUser(body, avatar);
 
-	if (
-		!body.first ||
-		!body.last ||
-		!body.email ||
-		!body.password ||
-		!body.phone ||
-		!body.street ||
-		!body.streetNum ||
-		!body.departmentNum ||
-		!body.age
-	) {
-		return res.status(400).send({
-			status: 'Bad Request',
-			message: 'Falta informacion, campos obligatorios',
-			data: newUser,
-		});
-	} else {
-		try {
-			await userService.createNewUser(newUser);
-
-			res.redirect('/users/login');
-		} catch (error) {
-			throw error;
-		}
+		res.redirect('/users/login');
+	} catch (error) {
+		console.log(error);
 	}
 };
 
