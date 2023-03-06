f6dbf5cdf1d91fc21bc60a14f2485b56cd6b620a
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-06 20:47:11 -0300

    feat: :zap: add DTO for orders

---
+++ src/dto/Order/OrdersDTO.js
@@ -0,0 +1,19 @@
+import { ordersModel } from '../../schemas/index.js';
+
+export const ordersFormatSchemaDTO = (user, cartById) => {
+	const newOrder = new ordersModel({
+		user: {
+			firstName: user.name.first,
+			lastName: user.name.last,
+		},
+		userId: user.name._id,
+		address: {
+			street: user.address.street,
+			streetNum: user.address.streetNum,
+			departmentNum: user.address.departmentNum,
+		},
+		products: cartById.products,
+	});
+
+	return newOrder;
+};
