fb277dba9d281d22829271a7a8653f461095624f
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-06 20:49:35 -0300

    refactor: :zap: separate logic from sending mail using orderEmail

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,57 +1,31 @@
 import * as Orders from '../daos/orders/OrdersDAOMongo.js';
 import * as Carts from '../daos/carts/CartsDAOMongo.js';
-import { transporter, sendWhatsapp } from '../config/index.js';
+import { sendWhatsapp, logger } from '../config/index.js';
 import { cartsModel } from '../schemas/index.js';
+import { orderEmail } from '../templates/orderEmail.js';
 
 export const getAllOrders = async (limit, since) => {
-	const allOrders = await Orders.getAllOrders(limit, since);
-	return allOrders;
+	try {
+		const allOrders = await Orders.getAllOrders(limit, since);
+		return allOrders;
+	} catch (error) {
+		logger.error(error);
+	}
 };
 
 export const createNewOrder = async (idCart, user) => {
-	const cartBuy = await cartsModel.findById(idCart).lean();
-	if (cartBuy === null) return { message: 'Carrito no encontrado' };
-	const savedOrder = await Orders.createNewOrder(idCart, user);
+	try {
+		const cartBuy = await cartsModel.findById(idCart).lean();
+		if (cartBuy === null) return { message: 'Carrito no encontrado' };
+		const savedOrder = await Orders.createNewOrder(idCart, user);
 
-	let prodDetail = '';
-	savedOrder.products.forEach(prod => {
-		prodDetail += `
-			<ul>
-				<li>
-					<strong>Producto: </strong>${prod.title}
-				</li>
-				<li>
-					<strong>Precio: </strong>$${prod.price}
-				</li>
-				<li>
-					<strong>Cantidad: </strong>${prod.quantity}
-				</li>
-				<li>
-					<strong>Total: </strong>$${prod.quantity}
-				</li>
-			</ul>`;
-	});
+		await orderEmail(savedOrder, user);
 
-	await transporter.sendMail({
-		from: `"Nodemailer ⚡" <${process.env.NODEMAILER_EMAIL}>`,
-		to: `${process.env.NODEMAILER_EMAIL_ADMIN}`,
-		subject: 'Nuevo pedido 🤑',
-		html: `
-			<h1>Nuevo pedido 🔥</h1>
-			<hr/>
-			<h3>Informacion:</h3>
-			<h4><strong>Nro de Orden: </strong> ${savedOrder._id}</h4>
-			<h4><strong>Nombre: </strong> ${user.name.first} ${user.name.last}</h4>
-			<h4><strong>Id usuario: </strong> ${savedOrder.userId}</h4>
-			<h4><strong>Fecha: </strong> ${savedOrder.createdAt}</h4>
-			<h4><strong>Productos: </strong></h4>
-			<div>${prodDetail}</div>
-			<hr/>
-			`,
-	});
+		await sendWhatsapp(process.env.PHONE_TEST, savedOrder._id); // --> change process.env.PHONE_TEST for user.phone to send whatsapp to client
 
-	sendWhatsapp(process.env.PHONE_TEST, savedOrder._id); // --> change process.env.PHONE_TEST for user.phone ti send whatsapp to client
-
-	await Carts.deletCart(idCart);
-	return savedOrder;
+		await Carts.deletCart(idCart);
+		return savedOrder;
+	} catch (error) {
+		logger.error(error);
+	}
 };
