1142400889e63fa56a20c275b6670946d4ad2773
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-06 20:46:19 -0300

    refactor: :recycle: add DTO logic to form object
    use ordersFormatSchemaDTO

--- src/daos/orders/OrdersDAOMongo.js
+++ src/daos/orders/OrdersDAOMongo.js
@@ -1,40 +1,38 @@
+import { ordersFormatSchemaDTO } from '../../dto/Order/OrdersDTO.js';
 import { ordersModel, cartsModel } from '../../schemas/index.js';
+import { logger } from '../../config/index.js';
 
 export const getAllOrders = async (limit = 10, since = 0) => {
-	const allOrders = await productsModel
-		.find({})
-		.limit(Number(limit))
-		.skip(Number(since));
-	const totalOrders = await ordersModel.countDocuments({});
-	return {
-		totalOrders,
-		allOrders,
-	};
+	try {
+		const allOrders = await productsModel
+			.find({})
+			.limit(Number(limit))
+			.skip(Number(since));
+		const totalOrders = await ordersModel.countDocuments({});
+		return {
+			totalOrders,
+			allOrders,
+		};
+	} catch (error) {
+		logger.error(error);
+	}
 };
 
 export const createNewOrder = async (idCart, user) => {
-	const cartById = await cartsModel.findById(idCart).lean();
-	if (cartById === null) {
-		return {
-			message: `El carrito con Id ${idCart} no fue encontrado`,
-		};
+	try {
+		const cartById = await cartsModel.findById(idCart).lean();
+		if (cartById === null) {
+			return {
+				message: `El carrito con Id ${idCart} no fue encontrado`,
+			};
+		}
+
+		const newOrder = ordersFormatSchemaDTO(user, cartById);
+
+		await newOrder.save();
+		const seeNewOrder = await ordersModel.findOne({ userId: user.name._id });
+		return seeNewOrder;
+	} catch (error) {
+		logger.error(error);
 	}
-
-	const newOrder = new ordersModel({
-		user: {
-			firstName: user.name.first,
-			lastName: user.name.last,
-		},
-		userId: user.name._id,
-		address: {
-			street: user.address.street,
-			streetNum: user.address.streetNum,
-			departmentNum: user.address.departmentNum,
-		},
-		products: cartById.products,
-	});
-
-	await newOrder.save();
-	const seeNewOrder = await ordersModel.findOne({ userId: user.name._id });
-	return seeNewOrder;
 };
